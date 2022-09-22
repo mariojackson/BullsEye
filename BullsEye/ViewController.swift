7e3fdcd74133ba740d89362e748fee4bebe56c6a
Mario Jackson <mario@jackson.softwre>
2022-09-22 20:15:13 +0200

    Use initial slider value defined in storyboard

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 50
+    @IBOutlet var slider: UISlider!
+    var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentValue = lroundf(slider.value)
     }
 
     @IBAction func showAlert() {
