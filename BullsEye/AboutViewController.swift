1d28af0bf0afad6046c1a1d7a4e8dc94755b430e
Mario Jackson <mario@jackson.softwre>
2022-09-23 22:59:41 +0200

    About scene

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Mario Jackson on 9/23/22.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true)
+    }
+    
+}
